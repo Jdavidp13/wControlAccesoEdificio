22b5cd758a9fd76105c07c4b33b3b0af72660f95
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-19 18:15:16 -0500

    Repositorio para visitantes utilizando interfaz

---
+++ wControlAccesoEdificio/Data/Repository/IVisitantesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wControlAccesoEdificio.Entities;
+
+namespace wControlAccesoEdificio.Data.Repository
+{
+    public interface IVisitantesRepository
+    {
+        List<Visitante> GetAll();
+        void Añadir(Visitante visitante);
+        void Actualizar(Visitante visitante);
+        void Eliminar(int visitanteID);
+    }
+}
