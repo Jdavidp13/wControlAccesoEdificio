6a4fba39bfd947e5a925c225f9329b72ebec5412
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-19 18:05:15 -0500

    Clase modelo para Visitantes

---
+++ wControlAccesoEdificio/Entities/Visitante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wControlAccesoEdificio.Entities
+{
+    public class Visitante
+    {
+        public int VisitanteID { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string MotivoVisita { get; set; }
+    }
+}
