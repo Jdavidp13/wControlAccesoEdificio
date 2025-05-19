9f56be92d9b9866ae6206be755429483af559acb
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-19 17:55:55 -0500

    Conexion a base de datos singleton

---
+++ wControlAccesoEdificio/Utils/DBconnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace wControlAccesoEdificio.Utils
+{
+    public sealed class DBconnection
+    {
+        private static readonly DBconnection instancia = new DBconnection();
+        private readonly SqlConnection connection;
+        private DBconnection()
+        {
+            string connSr = ConfigurationManager.ConnectionStrings["DB_ControlAccesoEdificio"].ConnectionString;
+            connection = new SqlConnection(connSr);
+        }
+        public static DBconnection Instancia => instancia;
+        public SqlConnection GetSqlConnection()
+        {
+            return connection; 
+        }
+    }
+}
