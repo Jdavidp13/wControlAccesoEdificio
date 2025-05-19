e116dd2bd6d15b84645c144bf76383fc9ceeece1
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-19 18:12:22 -0500

    Repositorio para empleados utilizando la interfaz

---
+++ wControlAccesoEdificio/Data/Repository/IEmpleadoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wControlAccesoEdificio.Entities;
+
+namespace wControlAccesoEdificio.Data.Repository
+{
+    public interface IEmpleadoRepository
+    {
+        List<Empleado> GetAll();
+        void Añadir(Empleado empleado);
+        void Actualizar(Empleado empleado);
+        void Eliminar(int empleadoID);
+    }
+}
