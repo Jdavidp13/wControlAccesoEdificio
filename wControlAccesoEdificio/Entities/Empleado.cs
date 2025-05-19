cdf7a3066c41b0e99e017d8893a8c36940e12bef
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-19 18:03:23 -0500

    clase modelo Empleado

---
+++ wControlAccesoEdificio/Entities/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wControlAccesoEdificio.Entities
+{
+    public class Empleado
+    {
+        public int EmpleadoID { get; set; }
+        public string Nombre { get; set; }
+        public string Rol { get; set; }
+        public int ZonaAcceso { get; set; }
+    }
+}
